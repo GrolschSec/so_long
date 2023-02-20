e3560f59b80dbaed1758283d1f6254ac2f2abe51
Grolsch Information Security <grolsch-sec@protonmail.com>
2023-02-20 20:54:30 +0100

    Norm

--- srcs/chained.c
+++ srcs/chained.c
@@ -6,32 +6,32 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 20:09:25 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/02/20 20:46:03 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/02/20 20:54:15 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-void	add_node(t_collect *head, int row, int col, t_game *game) 
+void	add_node(t_collect *head, int row, int col, t_game *game)
 {
-    t_collect *new_node;
-	t_collect *current;
-	
+	t_collect	*new_node;
+	t_collect	*current;
+
 	new_node = malloc(sizeof(t_collect));
 	if (!new_node)
 		return ;
-    new_node->loc = (t_point){ .row = row, .col = col };
+	new_node->loc = (t_point){.row = row, .col = col};
 	new_node->visited = 0;
-    new_node->next = NULL;
-    if (head == NULL)
+	new_node->next = NULL;
+	if (head == NULL)
 	{
-        game->collect = new_node;
-    }
+		game->collect = new_node;
+	}
 	else
 	{
 		current = head;
 		while (current->next != NULL)
 			current = current->next;
-        current->next = new_node;
-    }
+		current->next = new_node;
+	}
 }
